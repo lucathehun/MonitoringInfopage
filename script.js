241b1458be970c55d532d3f3c3a83a3a46ca28eb
lucathehun <luca@gatty.dev>
2024-07-25 14:01:49 +0200

    dynamic sheeesh

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
 
 function toggleMenu(x) {
     x.classList.toggle("change");
-    document.getElementById("menuContent").classList.toggle("show");
+    const menuContent = document.getElementById("menuContent");
+    menuContent.classList.toggle("show");
 }
 
-// Load the body content of index.html into the menu
-fetch('menu.html')
+fetch('basicInfo.html')
     .then(response => response.text())
     .then(data => {
         const parser = new DOMParser();
         const doc = parser.parseFromString(data, 'text/html');
         const bodyContent = doc.body.innerHTML;
-        document.getElementById('menuContent').innerHTML = bodyContent;
-    })
-    .catch(error => console.error('Error loading menu.html:', error));
+        document.getElementById('basic-content').innerHTML = bodyContent;})
+
+fetch('advancedInfo.html')
+    .then(response => response.text())
+    .then(data => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(data, 'text/html');
+        const bodyContent = doc.body.innerHTML;
+        document.getElementById('advanced-content').innerHTML = bodyContent;})
+
+
+function showBasicContent() {
+    // Set display to flex for basic-content
+    document.getElementById('basic-content').style.display = 'flex';
+    
+    // Set display to none for advanced-content and main-content
+    document.getElementById('advanced-content').style.display = 'none';
+    document.getElementById('main-content').style.display = 'none';
+}
+
+function showAdvancedContent() {
+    // Set display to flex for basic-content
+    document.getElementById('basic-content').style.display = 'none';
+    
+    // Set display to none for advanced-content and main-content
+    document.getElementById('advanced-content').style.display = 'flex';
+    document.getElementById('main-content').style.display = 'none';
+}
+
+function showMainContent() {
+    // Set display to flex for basic-content
+    document.getElementById('basic-content').style.display = 'none';
+    
+    // Set display to none for advanced-content and main-content
+    document.getElementById('advanced-content').style.display = 'none';
+    document.getElementById('main-content').style.display = 'flex';
+}
